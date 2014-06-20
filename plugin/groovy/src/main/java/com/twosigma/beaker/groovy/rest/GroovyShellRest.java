41cda7367878f28b10bdd990f70a1723b289f382
anglee <ang.lee@gmail.com>
2014-06-20 16:43:32 -0400

    Make _out available in Groovy Plugin

--- plugin/groovy/src/main/java/com/twosigma/beaker/groovy/rest/GroovyShellRest.java
+++ plugin/groovy/src/main/java/com/twosigma/beaker/groovy/rest/GroovyShellRest.java
@@ -19,8 +19,11 @@ import com.google.inject.Singleton;
 import com.twosigma.beaker.jvm.object.SimpleEvaluationObject;
 import groovy.lang.GroovyShell;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -29,11 +32,15 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpVersion;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.entity.ContentType;
 import org.codehaus.groovy.control.CompilationFailedException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 @Path("groovysh")
 @Produces(MediaType.APPLICATION_JSON)
@@ -43,6 +50,8 @@ public class GroovyShellRest {
   private final Map<String, GroovyShell> shells = new HashMap<>();
 
   public GroovyShellRest() throws IOException {}
+
+  private JSONParser parser = new JSONParser();
 
   @POST
   @Path("getShell")
@@ -63,13 +72,76 @@ public class GroovyShellRest {
   public SimpleEvaluationObject evaluate(
       @FormParam("shellId") String shellId,
       @FormParam("code") String code,
-      @FormParam("evalId") String evalId) throws InterruptedException, IOException, ClientProtocolException {
+      @FormParam("evalId") String evalId) throws InterruptedException, IOException, ClientProtocolException, ParseException {
+
+    long beginTime = new Date().getTime();
+
+
+
+    String last = "null";
+    List<String> _keys = new ArrayList<String>();
+    List<String> _out = new ArrayList<String>();
+    int thisIndex = -1;
+    {
+      String response = Request.Get("https://glaring-fire-5327.firebaseIO.com/_evaluations.json")
+        .useExpectContinue()
+        .version(HttpVersion.HTTP_1_1)
+        .connectTimeout(1000)
+        .socketTimeout(1000)
+        .execute().returnContent().asString();
+      //System.out.println("\n\nevaluations = \n" + response);
+
+      Object obj = this.parser.parse(response);
+      JSONObject evaluators = (JSONObject) obj;
+      Iterator iter = evaluators.entrySet().iterator();
+      while (iter.hasNext()) {
+        Map.Entry entry = (Map.Entry) iter.next();
+        _keys.add((String) entry.getKey());
+      }
+      Collections.sort(_keys);
+      //System.out.println("keys = [" + StringUtils.join(_keys, ", ") + "]");
+      thisIndex = _keys.indexOf(evalId);
+      //System.out.println("thisIndex = " + thisIndex);
+
+      Iterator<String> it2 = _keys.iterator();
+      while (it2.hasNext()) {
+        JSONObject eval = (JSONObject) evaluators.get(it2.next());
+        JSONObject output = (JSONObject) eval.get("output");
+        //JSONObject result = (JSONObject) output.get("result");
+        _out.add(output.get("result").toString());
+      }
+    }
+    {
+      String body = "{" +
+          "\"begin_time\":" + beginTime + "," +
+          "\"result\": \"evaluating\"" + "," +
+          "\"evalId\":" + "\"" + evalId + "\"," +
+          "\"eid\":" + thisIndex +
+          "}";
+      //System.out.println("body1 = \n" + body);
+      String response = Request.Put("https://glaring-fire-5327.firebaseIO.com/_evaluations/" + evalId + "/output.json")
+        .useExpectContinue()
+        .version(HttpVersion.HTTP_1_1)
+        .bodyString(body, ContentType.DEFAULT_TEXT)
+        .execute().returnContent().asString();
+    }
 
     SimpleEvaluationObject obj = new SimpleEvaluationObject(code);
     obj.started();
     GroovyShell shell = getEvaluator(shellId);
+    Object _last = (_out.size() > 1) ? _out.get(_out.size() - 2) : null;
+    Map<String, Object> bk = new HashMap<String, Object>();
+    bk.put("$_", _last);
+    bk.put("_out", _out);
+    shell.setVariable("bk", bk);
+
     Object result;
     try {
+//      String preEval = "bk=[$_:" + last + "]; thisIndex = " + thisIndex + "; "
+//          + "keys = [\"" + StringUtils.join(_keys, "\", \"") + "\"];"
+//          + "_out = [\"" + StringUtils.join(_out, "\", \"") + "\"];";
+//      System.out.println("preEval\n" + preEval);
+      //shell.evaluate(preEval);
       result = shell.evaluate(code);
     } catch (Exception e) {
       obj.error(e);
@@ -77,20 +149,16 @@ public class GroovyShellRest {
     }
     obj.finished(result);
     {
-      //String body = "{\"output\": {\"result\": \"" + result + "\"}}";
-      //String response = Request.Put("https://glaring-fire-5327.firebaseIO.com/_evaluations/" + evalId + ".json")
-      String body = "\"" + result + "\"";
-      String response = Request.Put("https://glaring-fire-5327.firebaseIO.com/_evaluations/" + evalId + "/output/result.json")
-        .useExpectContinue()
-        .version(HttpVersion.HTTP_1_1)
-        .bodyString(body, ContentType.DEFAULT_TEXT)
-        .execute().returnContent().asString();
-    }
-    {
-      //String body = "{\"output\": {\"result\": \"" + result + "\"}}";
-      //String response = Request.Put("https://glaring-fire-5327.firebaseIO.com/_evaluations/" + evalId + ".json")
-      String body = "\"" + new Date().getTime() + "\"";
-      String response = Request.Put("https://glaring-fire-5327.firebaseIO.com/_evaluations/" + evalId + "/output/finish_time.json")
+      long endTime = new Date().getTime();
+      String body = "{" +
+          "\"begin_time\":" + beginTime + "," +
+          "\"result\": \"" + result + "\"" + "," +
+          "\"evalId\":" + "\"" + evalId + "\"" + "," +
+          "\"eid\":" + thisIndex + "," +
+          "\"end_time\":" + "\"" + endTime + "\"" +
+          "}";
+      //System.out.println("body2 = \n" + body);
+      String response = Request.Put("https://glaring-fire-5327.firebaseIO.com/_evaluations/" + evalId + "/output.json")
         .useExpectContinue()
         .version(HttpVersion.HTTP_1_1)
         .bodyString(body, ContentType.DEFAULT_TEXT)
