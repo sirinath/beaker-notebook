3b1a789d2f1c754f65714fe809ca07ac38d6e6f0
anglee <ang.lee@gmail.com>
2014-06-20 09:24:27 -0400

    Reformat node/app.js

--- plugin/node/src/dist/app/app.js
+++ plugin/node/src/dist/app/app.js
@@ -16,43 +16,43 @@ app.use(express.urlencoded()); // to support URL-encoded bodies
 app.use(express.basicAuth('beaker', process.env.beaker_plugin_password));
 
 // route for testing service is alive
-app.get('/pulse', function(request, response){
-    response.send('node server is running');
+app.get('/pulse', function (request, response) {
+  response.send('node server is running');
 });
 
-app.post('/shell', function(request, response){
-    var returnObject = {'shellID':uuid.v4()};
-    response.setHeader('Content-Type', 'application/json');
-    response.send(JSON.stringify(returnObject));
+app.post('/shell', function (request, response) {
+  var returnObject = {'shellID': uuid.v4()};
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify(returnObject));
 });
 
-app.post('/evaluate', function(request, response){
-    var shellID = request.body.shellID;
-    var code =  decodeURIComponent(request.body.code);
-    var evaluationResult = processCode(code);
-    if (evaluationResult.processed){
-        response.statusCode = 200;
-    } else {
-        response.statusCode = 422;
-    }
-    response.send(evaluationResult.evaluation.toString());
+app.post('/evaluate', function (request, response) {
+  var shellID = request.body.shellID;
+  var code = decodeURIComponent(request.body.code);
+  var evaluationResult = processCode(code);
+  if (evaluationResult.processed) {
+    response.statusCode = 200;
+  } else {
+    response.statusCode = 422;
+  }
+  response.send(evaluationResult.evaluation.toString());
 });
 
 function processCode(code) {
-    var returnValue;
-    var result;
-    try {
-        result = eval(code);
-        if(typeof result === "undefined"){
-            result =  'undefined';
-        }
-        returnValue = {evaluation:result,
-                       processed:true};
-    } catch (e) {
-        returnValue = {evaluation:'Error: ' + e.message + '\n' + e.stack,
-                       processed:false};
+  var returnValue;
+  var result;
+  try {
+    result = eval(code);
+    if (typeof result === "undefined") {
+      result = 'undefined';
     }
-    return returnValue;
+    returnValue = {evaluation: result,
+      processed: true};
+  } catch (e) {
+    returnValue = {evaluation: 'Error: ' + e.message + '\n' + e.stack,
+      processed: false};
+  }
+  return returnValue;
 }
 
 app.listen(port, host);
